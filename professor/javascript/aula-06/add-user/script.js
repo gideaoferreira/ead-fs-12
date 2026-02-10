0d222734c632a7c3a28e88ede73608e994b6e21c
Gideão Ferreira <gideaoferreira@hotmail.com>
2026-02-09 22:08:53 -0300

    Adicionada a funcionalidade do localstorage e remoção de item usando o metodo filter do array

--- professor/javascript/aula-06/add-user/script.js
+++ professor/javascript/aula-06/add-user/script.js
@@ -1,32 +1,101 @@
 const btnAddUser = document.getElementById('btn-add-user')
+const tableBody = document.getElementById('table-body')
+let users = []
+let formIsValid = true
+
+let data = JSON.parse(localStorage.getItem('users'))
+
+function deleteUser(cpf) {
+    users = users.filter(function (user) {
+        if (user.cpf !== cpf) {
+            return user
+        }
+    })
+    console.log(users)
+
+    localStorage.setItem('users', JSON.stringify(users))
+    tableBody.innerHTML = ''
+    users.forEach(function (user) {
+        const tr = document.createElement('tr')
+            tr.innerHTML = `
+                <td> ${user.name} </td>
+                <td> ${user.surname} </td>
+                <td> ${user.birthdate} </td>
+                <td> ${user.email} </td>
+                <td> ${user.phone} </td>
+                <td> ${user.cpf} </td>
+                <td> <button class="btn btn-danger btn-sm" type="button" onclick="deleteUser('${user.cpf}')"> Deletar </button></td>
+            `
+        tableBody.appendChild(tr)
+    })
+}
+
+if (data) {
+    data.forEach(function (user) {
+        users.push(user)
+        const tr = document.createElement('tr')
+            tr.innerHTML = `
+                <td> ${user.name} </td>
+                <td> ${user.surname} </td>
+                <td> ${user.birthdate} </td>
+                <td> ${user.email} </td>
+                <td> ${user.phone} </td>
+                <td> ${user.cpf} </td>
+                <td> <button class="btn btn-danger btn-sm" type="button" onclick="deleteUser('${user.cpf}')"> Deletar </button></td>
+            `
+        tableBody.appendChild(tr)
+    })
+}
 
 btnAddUser.addEventListener('click', function () {
+    formIsValid = true
     const forAddUser = document.getElementById('form-add-user')
-
+    
+    // Conversão de dados da tabela para estrair o objeto com chave:valor
     const formData = new FormData(forAddUser)   
     const data = Object.fromEntries(formData)
 
-    const newDate = new Date(data.birthdate)
+    Object.keys(data).forEach(function (key) {
+        if (document.getElementById(`${key}-message-error`)) {
+            document.getElementById(`${key}-message-error`).classList.add('d-none')
+        }
+    })
+    
+    Object.keys(data).forEach(function (key) {
+        const value = data[key]
+        if (value === '' || value === null) {
+            formIsValid = false
+            if (document.getElementById(`${key}-message-error`)) {
+                document.getElementById(`${key}-message-error`).classList.remove('d-none')
+            }
+        }
+    })
 
-    const tmzDiff = new Date().getTimezoneOffset() / 60
-    const localTmz = 24 + tmzDiff
+    console.log(formIsValid)
 
-    const birthdateToDateFormat = new Date(Date.UTC(newDate.getFullYear(), newDate.getMonth(), newDate.getDate(), localTmz))
+    if (!formIsValid) {
+        return
+    }
 
-    const dateBirthdateFormated = new Intl.DateTimeFormat("pt-BR").format(birthdateToDateFormat)
+    // Adiciona o usuário ao array e logo depois ao Localstorage convertendo os dados com o JSON.stringify
+    users.push(data)
+    localStorage.setItem('users', JSON.stringify(users))
 
-    const tableBody = document.getElementById('table-boby')
+    // Cria um elemento html "tr" dinamicamente
     const tr = document.createElement('tr')
 
+    // Adiciona celulas a "tr" criada anteriormente
     tr.innerHTML = `
         <td> ${data.name} </td>
         <td> ${data.surname} </td>
-        <td> ${dateBirthdateFormated} </td>
+        <td> ${data.birthdate} </td>
         <td> ${data.email} </td>
         <td> ${data.phone} </td>
         <td> ${data.cpf} </td>
+        <td> <button class="btn btn-danger btn-sm" type="button" onclick="deleteUser('${data.cpf}')"> Deletar </button></td>
     `
-
+    // Adiciona a "tr" ao tboby
     tableBody.appendChild(tr)
+    // Limpa o form
     forAddUser.reset()
 })
