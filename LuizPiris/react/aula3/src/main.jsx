30935cf29cd47ca846aa377717ebe6015eeaed35
LuizPiris <luizhpiris@gmail.com>
2026-03-11 18:41:16 -0300

    correção aula 03 de react

--- LuizPiris/react/aula3/src/main.jsx
+++ LuizPiris/react/aula3/src/main.jsx
@@ -4,6 +4,10 @@ import { RouterProvider} from 'react-router-dom';
 import './assets/style/main.scss';
 import "bootstrap";
 import "@popperjs/core";
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
 import router from './routes';
 
 createRoot(document.getElementById('root')).render(
